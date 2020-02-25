1c82c55a7e4643383710c51eb280a18f62cb4d5b
Yari Adan Petralanda <mustang@yadan.org>
2020-02-25 04:59:35 +0100

    Checking only borders.

--- RushBSQ/rushbsq.c
+++ RushBSQ/rushbsq.c
@@ -26,11 +26,36 @@
 ** x - left ot right, y - up/down
 */
 
-static int is_square_valid(t_map *map, unsigned int row, unsigned int column,
-			   unsigned int increase, int full_check)
+static int is_square_valid_borders(t_map *map, unsigned int row,
+				   unsigned int column, unsigned int increase)
 {
-	(void) full_check;
+	unsigned int row_scan;
+	unsigned int column_scan;
 
+	if (((row + increase) > map->height) ||
+	    ((column + increase) > map->width))
+		return 0; // Square would be out of bounds.
+
+	row_scan = row; // Scan vertically for obstacles.
+	while (row_scan < row + increase) {
+		if (map_is_obstacle(map, row_scan, column + increase - 1))
+			return 0; // Obstacle found.
+		row_scan++;
+	}
+
+	column_scan = column; // Scan horizontally for obstacles.
+	while (column_scan < column + increase) {
+		if (map_is_obstacle(map, row + increase - 1, column_scan))
+			return 0; // Obstacle found.
+		column_scan++;
+	}
+
+	return 1; // Square fits OK.
+}
+
+static int is_square_valid_full(t_map *map, unsigned int row,
+				unsigned int column, unsigned int increase)
+{
 	unsigned int row_scan;
 	unsigned int column_scan;
 
@@ -57,21 +82,22 @@ static int is_square_valid(t_map *map, unsigned int row, unsigned int column,
 static void point_check(t_map *map, unsigned int row, unsigned int column,
 		 t_solution *solution)
 {
-	unsigned int increase; //increase index
-	int full_check;
+	unsigned int increase; // Increase index.
 
 	increase = solution->best + 1;
-	full_check = 1;
+
+	if (!is_square_valid_full(map, row, column, increase))
+		return;
+
 	while (1) // Try bigger and bigger squares.
 	{
-		if (!is_square_valid(map, row, column, increase, full_check))
-			return;
-
-		full_check = 0;
 		solution->best = increase;
 		solution->row = row;
 		solution->column = column;
 		increase++;
+
+		if (!is_square_valid_borders(map, row, column, increase))
+			return;
 	}
 }
 
@@ -101,6 +127,7 @@ static void solve(t_map *map, t_solution *solution)
 static void solve_path(const char *path)
 {
 	t_solution solution;
+	solution.row = solution.column = 0;
 	t_map map;
 
 	if (0 != file_get_map(path, &map)) {
